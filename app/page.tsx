4ad0adcf629054ba4134989c8cabcf0e830c1f7b
lol-laughter <i-yuhei729@i.softbank.jp>
2024-10-26 16:49:10 +0900

    スタートボタンを追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import ReactHowler from 'react-howler';
 import { Destination } from './component/destination';
 import { getRandomLocation } from './lib/address';
 import MapView from './component/mapView';
 import { useCurrentLocation } from './hooks/useCurrentLocation';
 import Image from 'next/image';
+import { Button } from './component/button';
 
 export default function Home() {
+  const [start, setStart] = useState<boolean>(false);
+
   const radius = 5000;
   const currentLocation = useCurrentLocation();
   if (!currentLocation.location) return;
@@ -17,21 +20,39 @@ export default function Home() {
 
   return (
     <main
-      className="w-screen h-screen font-[family-name:var(--font-cezannePro-m)]"
+      className="w-screen h-screen font-[family-name:var(--font-cezannePro-m)] overflow-hidden"
       style={{ backgroundImage: "url('/images/wallPaper.jpg')" }}
     >
       <div className="w-screen h-screen flex flex-col gap-y-2 items-center justify-center">
-        <Image
-          src={'/images/title.png'}
-          alt="♡お散歩だいしゅきクラブ♡"
-          layout="intrinsic"
-          width={650}
-          height={350}
-          objectFit="cover"
-          className="my-[-80px]"
-        />
-        <MapView startLocation={currentLocation.location} endLocation={randamLocation} />
-        <Destination location={randamLocation} />
+        {!start && (
+          <div role="button" className="flex flex-col items-center" onClick={() => setStart(true)}>
+            <Image
+              src={'/images/title.png'}
+              alt="♡お散歩だいしゅきクラブ♡"
+              layout="intrinsic"
+              width={800}
+              height={400}
+              objectFit="cover"
+              onClick={() => setStart(true)}
+            />
+            <span className="text-6xl font-bold text-white">散歩を開始する</span>
+          </div>
+        )}
+        {start && (
+          <>
+            <Image
+              src={'/images/title.png'}
+              alt="♡お散歩だいしゅきクラブ♡"
+              layout="intrinsic"
+              width={650}
+              height={350}
+              objectFit="cover"
+              className="my-[-80px]"
+            />
+            <MapView startLocation={currentLocation.location} endLocation={randamLocation} />
+            <Destination location={randamLocation} />
+          </>
+        )}
       </div>
       <ReactHowler src="/music/attackOnTitan.mp3" playing loop />
     </main>
